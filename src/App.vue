418b20426b77a8db77bfb404e485049403dc5521
chenyuqi990215 <10175101214@stu.ecnu.edu.cn>
2021-02-19 16:09:17 +0800

    add addPosting.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <AddPosting course_title=""></AddPosting>
   </div>
 </template>
 
 <script>
 
-import AddPosting from "@/components/AddPosting";
 export default {
   name: 'App',
-  components: {
-    AddPosting
-  }
 }
 </script>
 
